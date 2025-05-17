5659b61456764b9cbfb7793b5fde63617d4e1b27
Marc Addeo <marcaddeo@gmail.com>
2025-05-17 12:36:07 -0400

    Reorganize variable definitions so only secrets are in tfvars

--- tofu/staging/locals.tf
+++ tofu/staging/locals.tf
@@ -1,3 +1,34 @@
 locals {
+  proxmox = {
+    name         = "crimson"
+    cluster_name = "armada"
+    endpoint     = "https://crimson.addeo.net:8006/"
+    insecure     = true
+    api_token    = var.proxmox_api_token
+    username     = "ansible"
+    private_key  = "${path.module}/../../ansible/host_keys/id_rsa.ansible@addeo.net"
+  }
+
   pvc_volumes = try(jsondecode(base64decode(data.external.pvc_volumes.result.base64)), {})
+
+  rancher = {
+    endpoint  = "https://rancher.staging.addeo.net/v3"
+    insecure  = true
+    api_token = var.rancher_api_token
+  }
+
+  rancher_oidc_provider = {
+    clientId            = var.rancher_oidc_provider_client_id
+    clientSecret        = var.rancher_oidc_provider_client_secret
+    issuer              = "https://id.staging.addeo.net"
+    authEndpoint        = "https://id.staging.addeo.net/authorize"
+    tokenEndpoint       = "https://id.staging.addeo.net/api/oidc/token"
+    userInfoEndpoint    = "https://id.staging.addeo.net/api/oidc/userinfo"
+    jwksUrl             = "https://id.staging.addeo.net/.well-known/jwks.json"
+    rancherUrl          = "https://rancher.staging.addeo.net/verify-auth"
+    scope               = "openid profile email"
+    groupSearchEnabled  = true
+    allowedPrincipalIds = var.rancher_oidc_provider_allowed_principal_ids
+    accessMode          = "unrestricted"
+  }
 }
